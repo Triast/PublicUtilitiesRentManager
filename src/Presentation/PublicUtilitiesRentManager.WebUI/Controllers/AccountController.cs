86c4e07c0b0cb3db242c38bcbf281900419720de
Ihar Kamarets <igor.komarets@gmail.com>
2019-05-04 08:57:49 +0300

    Changed anchors to entity views style in layout and added adding role to registered user.

--- src/Presentation/PublicUtilitiesRentManager.WebUI/Controllers/AccountController.cs
+++ src/Presentation/PublicUtilitiesRentManager.WebUI/Controllers/AccountController.cs
@@ -90,6 +90,8 @@ namespace PublicUtilitiesRentManager.WebUI.Controllers
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    var createdUser = await _userManager.FindByIdAsync(user.Id);
+                    await _userManager.AddToRoleAsync(createdUser, "User");
                     //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     //var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     //await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
